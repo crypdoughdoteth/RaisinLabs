2623429be280249e69dec72aac96f6fa103f406f
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2022-12-28 01:07:50 -0500

    Unit Tests v1

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,12 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "lib/forge-std/src/Test.sol";
+import "../src/Raisin.sol";
+import "../src/TestToken.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+abstract contract HelperContract {
+    TestToken public tt;
+    constructor() {
+        tt = new TestToken();
+    }
 
-    function testExample() public {
-        assertTrue(true);
+}
+
+contract ContractTest is Test, HelperContract {
+    
+    RaisinCore public raisin;
+
+    function setUp() public {
+
+        raisin = new RaisinCore(address(this), msg.sender);  
+        tt.approve(address(raisin), 100e18);
+        raisin.whitelistToken(tt);
+        raisin.manageDiscount(address(this), 100);
+    }
+
+    //passing tests
+    function testHappyCase() public {
+        raisin.initFund(5e18, tt, address(this));
+        raisin.donateToken(tt, 0, 6e18);
+        raisin.endFund(0);
+        raisin.fundWithdraw(tt, 0);
+    }
+    function testBaseCase() public{
+        raisin.initFund(5e18, tt, address(this));
+        raisin.donateToken(tt, 0, 4e18);
+        raisin.endFund(0);
+        raisin.refund(tt, 0);
+    }
+
+    function testNil() public {
+        raisin.initFund(5e18, tt, address(this));
+        raisin.endFund(0);
     }
 }
